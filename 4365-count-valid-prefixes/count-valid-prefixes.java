75ffe3aa13455672c624dcaf0a6040f55db58cc5
sangamesh2k4 <154347327+sangamesh2k4@users.noreply.github.com>
2026-08-01 21:34:51 +0530

    Time: 1 ms (100.00%) | Memory: 44.2 MB (9.63%) - LeetSync

---
+++ 4365-count-valid-prefixes/count-valid-prefixes.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countValidPrefixes(String s) {
+
+        int zero = 0;
+        int one = 0;
+        int ans = 0;
+
+        for (char c : s.toCharArray()) {
+
+            if (c == '0')
+                zero++;
+            else
+                one++;
+
+            if (Math.abs(zero - one) <= 1)
+                ans++;
+        }
+
+        return ans;
+    }
+}
