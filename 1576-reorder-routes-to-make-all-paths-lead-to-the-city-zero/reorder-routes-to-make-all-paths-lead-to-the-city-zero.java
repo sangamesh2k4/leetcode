a84fdf39db2dffe5ad70e18a430decd7937b5e4b
sangamesh2k4 <154347327+sangamesh2k4@users.noreply.github.com>
2026-08-03 10:37:44 +0530

    Time: 35 ms (75.49%) | Memory: 114.6 MB (67.75%) - LeetSync

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -2,9 +2,9 @@ class Solution {
     int answer;
     void dfs(int node,List<List<int[]>> adj,boolean[] visited){
         visited[node]=true;
-        for(int[] next : adj.get(node)){
-            int neighbor=next[0];
-            int cost=next[1];
+        for(int[] edge:adj.get(node)){
+            int neighbor=edge[0];
+            int cost=edge[1];
             if(!visited[neighbor]){
                 answer+=cost;
                 dfs(neighbor,adj,visited);
@@ -17,13 +17,13 @@ class Solution {
         for(int i=0;i<n;i++){
             adj.add(new ArrayList<>());
         }
-        for(int[] edge :connections){
+        for(int[] edge : connections){
             int u=edge[0];
             int v=edge[1];
             adj.get(u).add(new int[]{v,1});
             adj.get(v).add(new int[]{u,0});
         }
         dfs(0,adj,visited);
-       return answer; 
+        return answer;
     }
 }
