7cc7890dfe291939109c130bf20e3019abd2729c
sangamesh2k4 <154347327+sangamesh2k4@users.noreply.github.com>
2026-08-07 10:29:50 +0530

    Time: 1 ms (86.56%) | Memory: 47.4 MB (44.25%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,29 +1,49 @@
 class Solution {
+        int[] parent;
+        int[] rank;
+        int find(int node){
+            if(parent[node]==node){
+                return node;
+            }
+            return parent[node]=find(parent[node]);
+        }
+        void union(int u,int v){
+            int rootU=find(u);
+            int rootV=find(v);
 
-    void bfs(int start,int[][] isConnected, boolean[] visited){
-        Queue<Integer> q=new LinkedList<>();
-        visited[start]=true;
-        q.offer(start);
-        while(!q.isEmpty()){
-            int curr=q.poll();
-            for(int j=0;j<isConnected.length;j++){
-                if(isConnected[curr][j]==1 && !visited[j]){
-                   visited[j]=true;
-                   q.offer(j); 
+            if(rootU==rootV){
+                return;
+            }
+            if(rank[rootU]>rank[rootV]){
+                parent[rootV]=rootU;
+            }
+            else if(rank[rootV]>rank[rootU]){
+                parent[rootU]=rootV;
+            }
+            else{
+                parent[rootV]=rootU;
+                rank[rootU]++;
+            }
+        }
+    public int findCircleNum(int[][] isConnected) { 
+        int n=isConnected.length;
+        parent=new int[n];
+        rank =new int[n];
+
+        int provinces=n;
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+        }
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(isConnected[i][j]==1){
+                    if(find(i)!=find(j)){
+                        union(i,j);
+                        provinces--;
+                    }
                 }
             }
         }
-    }
-    public int findCircleNum(int[][] isConnected) {
-        int provience=0;
-        int n=isConnected.length;
-        boolean[] visited=new boolean[n];
-        for(int i=0;i<n;i++){
-            if(!visited[i]){
-                provience++;
-                bfs(i,isConnected,visited);
-            }
-        }
-        return provience;
+        return provinces;
     }
 }
