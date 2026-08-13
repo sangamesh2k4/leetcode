981cb398d3d631ab531c610c69dc765d5fb638da
sangamesh2k4 <154347327+sangamesh2k4@users.noreply.github.com>
2026-08-13 13:08:16 +0530

    Time: 350 ms (42.12%) | Memory: 98.5 MB (25.16%) - LeetSync

---
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -0,0 +1,55 @@
+class Solution {
+    int[] parent,rank;
+    int find(int node){
+        if(parent[node]==node){
+            return node;
+        }
+        return parent[node]=find(parent[node]);
+    }
+    void union(int u,int v){
+        int rootU=find(u);
+        int rootV=find(v);
+        if(rootU==rootV){
+            return;
+        }
+        if(rank[rootU]>rank[rootV]){
+            parent[rootV]=rootU;
+        }
+        else if(rank[rootV]>rank[rootU]){
+            parent[rootU]=rootV;
+        }
+        else {
+            parent[rootV]=rootU;
+            rank[rootU]++;
+        }
+    }
+    public int minCostConnectPoints(int[][] points) {
+        int n=points.length;
+        List<int[]> edges=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int cost=Math.abs(points[i][0]-points[j][0])
+                +Math.abs(points[i][1]-points[j][1]);
+                edges.add(new int[]{i,j,cost});
+            }
+        }
+        parent=new int[n];
+        rank=new int[n];
+        edges.sort((a,b)-> a[2]-b[2]);
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+        }
+        int cost=0,count=0;
+        for(int[] edge : edges){
+            if(find(edge[0])==find(edge[1])){
+                continue;
+            }
+            union(edge[0],edge[1]);
+            cost+=edge[2];
+            if(count==n-1){
+                break;
+            }
+        }
+        return cost;
+    }
+}
