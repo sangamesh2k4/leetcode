1bcf253b46d6a48e00429436800947386b43283f
sangamesh2k4 <154347327+sangamesh2k4@users.noreply.github.com>
2026-08-13 13:09:37 +0530

    Time: 318 ms (47.88%) | Memory: 98.3 MB (28.73%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -27,7 +27,7 @@ class Solution {
         int n=points.length;
         List<int[]> edges=new ArrayList<>();
         for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
+            for(int j=i+1;j<n;j++){
                 int cost=Math.abs(points[i][0]-points[j][0])
                 +Math.abs(points[i][1]-points[j][1]);
                 edges.add(new int[]{i,j,cost});
@@ -46,6 +46,7 @@ class Solution {
             }
             union(edge[0],edge[1]);
             cost+=edge[2];
+            count++;
             if(count==n-1){
                 break;
             }
