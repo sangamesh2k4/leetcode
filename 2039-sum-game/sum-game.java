7359cb6ba7b04a1bc48ca5921adb28f8526836af
sangamesh2k4 <154347327+sangamesh2k4@users.noreply.github.com>
2026-08-23 11:41:49 +0530

    Time: 8 ms (53.28%) | Memory: 46.8 MB (83.94%) - LeetSync

--- 2039-sum-game/sum-game.java
+++ 2039-sum-game/sum-game.java
@@ -3,8 +3,6 @@ class Solution {
         int n = num.length();
         int leftSum = 0, rightSum = 0;
         int qLeft = 0, qRight = 0;
-        
-        // First half
         for (int i = 0; i < n / 2; i++) {
             if (num.charAt(i) == '?') {
                 qLeft++;
@@ -12,8 +10,6 @@ class Solution {
                 leftSum += num.charAt(i) - '0';
             }
         }
-        
-        // Second half
         for (int i = n / 2; i < n; i++) {
             if (num.charAt(i) == '?') {
                 qRight++;
@@ -21,12 +17,8 @@ class Solution {
                 rightSum += num.charAt(i) - '0';
             }
         }
-        
         int diff = leftSum - rightSum;
         int qDiff = qLeft - qRight;
-        
-        // Alice wins if Bob cannot force diff to be 0
-        // Bob wins if 2 * diff == -9 * qDiff
         return 2 * diff != -9 * qDiff;
     }
 }
