af4807c95526730b56c2a06fabcd0595d73bf12e
sangamesh2k4 <154347327+sangamesh2k4@users.noreply.github.com>
2026-09-03 09:28:00 +0530

    Time: 29 ms (19.68%) | Memory: 45.9 MB (78.57%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.java
+++ 1558-course-schedule-iv/course-schedule-iv.java
@@ -6,6 +6,7 @@ class Solution {
         }
         for(int k=0;k<numCourses;k++){
             for(int i=0;i<numCourses;i++){
+                if(!dp[i][k]) continue;
                 for(int j=0;j<numCourses;j++){
                     dp[i][j]=dp[i][j]|| (dp[i][k]&& dp[k][j]);
                 }
