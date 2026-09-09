cafbf8c924748fbda375af9387546e2835531315
sangamesh2k4 <154347327+sangamesh2k4@users.noreply.github.com>
2026-09-09 06:10:29 +0530

    Time: 1 ms (99.36%) | Memory: 42.5 MB (71.15%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long countCommas(long n) {
+       long ans=0;
+       long start=1000;
+       long commas=1;
+       while(start<=n){
+        long end=Math.min(start*1000-1,n);
+        ans+=(end-start+1)*commas;
+        start*=1000;
+        commas++;
+       }
+       return ans; 
+    }
+}
